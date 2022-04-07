988728d36cfbf74e311995d7eb6b532272abf429
CallMeShreyas <shreyask404@gmail.com>
2022-04-07 15:47:25 +0530

    4th commit

--- emailapi/urls.py
+++ emailapi/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 from .router import router
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('', include('simple.urls'))
 ]
